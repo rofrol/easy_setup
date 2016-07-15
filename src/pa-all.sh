7dcfd8c18c1ef8729d3b2b590aaf664b2f9fba9b
Roman Frołow <rofrol@gmail.com>
2016-07-15 17:57:45 +0200

    npm i and stopping

--- src/pa-all.sh
+++ src/pa-all.sh
@@ -4,8 +4,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$DIR"
 
-node pa-oracle-setup.js
-node pa-npm-setup.js
-node jboss-setup.js
-node jboss-deploy.js
+if [ ! -d "$DIR/../node_modules" ]; then
+  echo 'run `npm i` in project root';
+  exit 1;
+fi
+
+node pa-oracle-setup.js && \
+node pa-npm-setup.js && \
+node jboss-setup.js && \
+node jboss-deploy.js && \
 node jboss-start.js
