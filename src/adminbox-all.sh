f9eeb468052c4d0f2733c3f67c80e5140e8b4e55
Roman Frołow <rofrol@gmail.com>
2016-07-20 14:18:41 +0200

    adminbox

---
+++ src/adminbox-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$DIR"
+
+if [ ! -d "$DIR/../node_modules" ]; then
+  echo 'run `npm i` in project root';
+  exit 1;
+fi
+
+node adminbox-oracle-setup.js && \
+node jboss-setup.js && \
+node jboss-deploy.js && \
+node jboss-start.js
