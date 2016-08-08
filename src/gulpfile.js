ffeb7cfa1978f1f95cb53a8957a2d60b275978f0
Roman Frołow <rofrol@gmail.com>
2016-08-08 15:50:57 +0200

    live reload

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -3,13 +3,14 @@ var config = require('./config.js');
 
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
-// var server = require('gulp-express');
 var lr = require('tiny-lr')();
 var path = require('path');
+var fs = require('fs-extra');
 
 var dir = path.dirname(path.dirname(config.projectWar())) + '/src/main/webapp';
 
 gulp.task('default', function () {
+    console.log('go to http://localhost:8126/');
     nodemon({
       script: 'server.js'
     })
@@ -20,12 +21,14 @@ gulp.task('default', function () {
     lr.listen(35729);
 
     gulp.watch(dir + '/**/*', function(event){
-        console.log('event.path:', event.path);
-      var fileName = path.relative('3000', event.path);
-      lr.changed({
-        body: {
-          files: [fileName]
+        if(fs.lstatSync(event.path).isFile()) {
+            console.log('event.path:', event.path);
+
+            lr.changed({
+              body: {
+                files: [event.path]
+              }
+            });
         }
-      });
     });
 });
